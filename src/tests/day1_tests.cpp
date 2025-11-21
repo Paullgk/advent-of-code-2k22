2fb5c94a9d185ec9dc976d65ee34d173d1a864c6
Paul Le Guen de Kerneizon <paul.leguendekerneizon@savoirfairelinux.com>
2025-11-21 16:14:02 +0100

    day1_tests: add extractDataLines test
    This test test that the tool extractDataLines method is working
    properly.
    
    Signed-off-by: Paul Le Guen de Kerneizon <paul.leguendekerneizon@savoirfairelinux.com>

--- src/tests/day1_tests.cpp
+++ src/tests/day1_tests.cpp
@@ -36,6 +36,27 @@ TEST(CheckDay1PartOne, findMaxElfCalories)
     }
 }
 
+TEST(extractDataLines, extractLines)
+{
+    string input_data_example;
+    string input_data;
+    vector<string> input_data_example_lines;
+    vector<string> input_data_lines;
+
+    Tools tool;
+
+    tool.readInputFile("src/day1/example", &input_data_example);
+    tool.readInputFile("src/day1/input", &input_data);
+
+    tool.extractLines(input_data_example, &input_data_example_lines);
+    tool.extractLines(input_data, &input_data_lines);
+
+    EXPECT_EQ(input_data_example_lines.size(), 14);
+    EXPECT_EQ(input_data_lines.size(), 2249);
+
+}
+
+
 TEST(CheckDay1PartTwo, top_three_most_caloried_elf)
 {
     Day1 day1;
@@ -52,6 +73,7 @@ TEST(CheckDay1PartTwo, top_three_most_caloried_elf)
         GTEST_ASSERT_EQ(top_three_most_caloried_elf, 45000);
     }
 }
+
 int main(int argc, char* argv[])
 {
     ::testing::InitGoogleTest(&argc, argv);
